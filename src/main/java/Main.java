5af9795660373515879433f982627a209beb4ce5
D A Balabanov <balabanovda@gmail.com>
2019-11-29 16:52:11 +0300

    Empty project for ci

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public boolean isBigger(int a, int b) {
+        return a > b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Main main = new Main();
+        System.out.println(main.isBigger(10, 5));
+    }
+}
