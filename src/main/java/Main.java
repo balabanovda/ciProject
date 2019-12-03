e3bfb8d0df7e219d7417e2e826b837f47aa1196f
D A Balabanov <balabanovda@gmail.com>
2019-12-03 11:43:16 +0300

    easy test project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
-    public boolean isBigger(int a, int b) {
+    public static boolean isBigger(int a, int b) {
         return a > b;
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Main main = new Main();
-        System.out.println(main.isBigger(10, 5));
+
+        System.out.println(isBigger(10, 5));
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,11 @@
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
-
+    @Test
+    public void shouldCreateDefaultArrayTest() {
+        assertEquals(Main.isBigger(10,5), true);
     }
+
+}
